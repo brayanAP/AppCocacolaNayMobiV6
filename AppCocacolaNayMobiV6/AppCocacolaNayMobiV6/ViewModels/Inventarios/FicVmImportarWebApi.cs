5e4aca015bcc3b95a269f3b08e61fe4a1b1b2e22
BRAYAN ULISSES ARIAS PEREZ <brulariaspe@gmail.com>
2018-10-15 06:54:31 -0600

    brayanAP

--- AppCocacolaNayMobiV6/AppCocacolaNayMobiV6/ViewModels/Inventarios/FicVmImportarWebApi.cs
+++ AppCocacolaNayMobiV6/AppCocacolaNayMobiV6/ViewModels/Inventarios/FicVmImportarWebApi.cs
@@ -13,8 +13,8 @@ namespace AppCocacolaNayMobiV6.ViewModels.Inventarios
 {
     public class FicVmImportarWebApi : INotifyPropertyChanged
     {
-        private string _FicTextAreaImpInv;
-        private ICommand _FicMecImportInv, _FicMecImportCat;
+        private string _FicTextAreaImpInv, _FicLabelIdInv;
+        private ICommand _FicMecImportIdInv,_FicMecImportInv, _FicMecImportCat;
 
         private IFicSrvNavigationInventario IFicSrvNavigationInventario;
         private IFicSrvImportarWebApi IFicSrvImportarWebApi;
@@ -28,10 +28,48 @@ namespace AppCocacolaNayMobiV6.ViewModels.Inventarios
         {
             get { return _FicTextAreaImpInv; }
         }
+
+        public string FicLabelIdInv
+        {
+            get { return _FicLabelIdInv; }
+            set
+            {
+                if (value != null) _FicLabelIdInv = value;
+            }
+        }
+
         public async void OnAppearing()
         {
 
         }//METODO QUE SE MANDA A LLAMAR EN LA VIEW
+
+        public ICommand FicMecImportIdInv
+        {
+            get
+            {
+                return _FicMecImportIdInv = _FicMecImportIdInv ??
+                      new FicVmDelegateCommand(FicMecImportInventarioId);
+            }
+        }//ESTE VENTO AGREGA EL COMANDO AL BOTON EN LA VIEW
+
+        private async void FicMecImportInventarioId()
+        {
+            try
+            {
+                if(_FicLabelIdInv.Length > 0)
+                {
+                    _FicTextAreaImpInv = await IFicSrvImportarWebApi.FicGetImportInventarios(int.Parse(_FicLabelIdInv));
+                    RaisePropertyChanged("FicTextAreaImpInv");
+                    await new Page().DisplayAlert("ALERTA", "Datos Actualizados.", "OK");
+                }
+                else await new Page().DisplayAlert("ALERTA", "ID NO VALIDO.", "OK");
+
+            }
+            catch (Exception e)
+            {
+                await new Page().DisplayAlert("ALERTA", e.Message.ToString(), "OK");
+            }
+        }
 
         public ICommand FicMecImportInv
         {
